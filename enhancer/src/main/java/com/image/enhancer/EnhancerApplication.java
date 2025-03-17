a83462c01054642ded96b1a59025514be85c524d
Kunal Kumar <98895427+kunalkumar0024@users.noreply.github.com>
2025-03-17 21:40:52 +0530

    Initial commit

---
+++ enhancer/src/main/java/com/image/enhancer/EnhancerApplication.java
@@ -0,0 +1,13 @@
+package com.image.enhancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhancerApplication.class, args);
+	}
+
+}
---
+++ enhancer/src/test/java/com/image/enhancer/EnhancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.image.enhancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
